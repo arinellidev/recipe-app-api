35d749cc2c961b206b3a6110790cb624acf4ba7a
Patrick Arinelli <arinpar@gmail.com>
2022-11-08 12:38:02 -0300

    usermodel

---
+++ recipe-app-api/app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract and result equals to 5"""
+    return y - x
